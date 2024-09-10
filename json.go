91a3ceb41d195382715534e6c7fea86f7e939e1d
bmeek0110 <brandon.meek.q2.com>
2024-09-10 03:32:38 -0500

    yes

--- json.go
+++ json.go
@@ -6,6 +6,18 @@ import (
 	"net/http"
 )
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
+}
+
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	dat, err := json.Marshal(payload)
 	if err != nil {
